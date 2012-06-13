2d78cf93a257bb3ebcff02bbcb0e60f926b81b40
Eward Song <eward.song@gmail.com>
2012-06-13 09:22:07 +0800

    合并分支

--- demo/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd ../
-IMGPATH = `pwd`
-IMG = $IMGPATH/active_03.png
-URL = http://localhost/ci/index.php?c=upload&m=do_upload
-#需要增加type=image/png来设置content-type属性
-curl -F userfile=@'more.png;type=image/png' --trace-ascii tarcefie http://10.13.32.27/ci/index.php?c=upload\&m=do_upload
