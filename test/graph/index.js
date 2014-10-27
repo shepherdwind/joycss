154b02adc5dc380af9bcd57c70130c72f6290e9b
翰文 <eward.song@gmail.com>
2014-10-27 20:32:40 +0800

    feat(graph): graph api auto select support add cli arguments --api

---
+++ test/graph/index.js
@@ -0,0 +1,24 @@
+'use strict';
+var co = require('co');
+require('should');
+
+var graph = require('../../lib/graph/index');
+
+describe('lib/graph/index.js', function(){
+  it('auto select api', co(function* (){
+    var api = graph();
+    api.NAME.should.eql('java');
+  }));
+
+  it('select php', co(function* (){
+    global.joycssApi = 'php';
+    var api = graph();
+    api.NAME.should.eql('php');
+  }));
+
+  it('select java', co(function* (){
+    global.joycssApi = 'java';
+    var api = graph();
+    api.NAME.should.eql('java');
+  }));
+});
