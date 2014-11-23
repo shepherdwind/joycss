461d3e59754d77492bf02d4d41c6231e1f7969b6
shepherdwind <eward.song@gmail.com>
2014-11-23 08:45:21 +0800

    chore(travis): change travis config, try install php and pngqunat on test vm

--- test/graph/index.js
+++ test/graph/index.js
@@ -3,14 +3,11 @@ var co = require('co');
 require('should');
 
 var graph = require('../../lib/graph/index');
-var IS_TRAVIS = process.env.TRAVIS;
 
 describe('lib/graph/index.js', function(){
   it('auto select api', co(function* (){
-    if (!IS_TRAVIS) {
-      var api = graph();
-      api.NAME.should.eql('php');
-    }
+    var api = graph();
+    api.NAME.should.eql('php');
   }));
 
   it('select php', co(function* (){
